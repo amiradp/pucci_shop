911b5ce308d4f015fa35b9176a6bbb6af3aa570e
amiradp <amiradp3@gmail.com>
2023-12-16 19:20:18 +0330

    add basic models for products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    Name = models.CharField(max_length=225)
+    Description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    slug = models.SlugField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
